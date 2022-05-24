6e50fc228980b919f826215e95ba1f2c4ea771cd
tuvsheezz <tuvshee0089@gmail.com>
2022-05-25 00:38:24 +0900

    202205

--- app/controllers/notifier_controller.rb
+++ app/controllers/notifier_controller.rb
@@ -1,29 +1,34 @@
 class NotifierController < ApplicationController
   def notify
     require 'json'
-    logger = Logger.new('log/notifier_log.log')
-    file = File.open(Rails.root.join("problemlist/#{DateTime.current.strftime('%Y%m')}.json"))
-    json_data = JSON.parse(file.read)
-
     today = DateTime.current.strftime('%Y/%m/%d')
+
+    json_data = {
+      "2022/05/22": ['https://atcoder.jp/contests/abc219/tasks/abc219_d'],
+      "2022/05/23": ['https://atcoder.jp/contests/abc236/tasks/abc236_d'],
+      "2022/05/24": ['https://atcoder.jp/contests/abc246/tasks/abc246_d'],
+      "2022/05/25": ['https://atcoder.jp/contests/abc132/tasks/abc132_d'],
+      "2022/05/26": ['https://atcoder.jp/contests/abc188/tasks/abc188_e'],
+      "2022/05/27": ['https://atcoder.jp/contests/abc160/tasks/abc160_e'],
+      "2022/05/28": ['https://atcoder.jp/contests/abc181/tasks/abc181_e'],
+      "2022/05/30": ['Amrii tee'],
+      "2022/05/31": ['Amrii tee']
+    }
 
     listfound = false
 
     json_data.each do |date, linklist|
-      next unless today == date
+      next unless today == date.to_s
 
       listfound = true
-      raise Errno::ENOENT unless linklist.any?
+      raise "#{today}: list empty" unless linklist.any?
 
       callwebhook(linklist.join("\n"), params[:discord])
     end
 
-    raise Errno::ENOENT unless listfound
-  rescue Errno::ENOENT
-    callwebhook('Problem list not found!', params[:discord])
+    raise "#{today}: list not found" unless listfound
   rescue StandardError => e
-    callwebhook('DAILY NOTIFIER BUG!!!', params[:discord])
-    logger.error e
+    callwebhook(e, params[:discord])
   end
 
   def callwebhook(msg, discord_url)
